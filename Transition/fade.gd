6b979472c45c8f091c6d8f975941775d0160883d
Raf <rafay.b075@gmail.com>
2024-12-14 18:33:09 -0600

    transitions and basic test level

---
+++ Transition/fade.gd
@@ -0,0 +1,42 @@
+extends Node
+
+signal on_transition_finished
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(color_rect)
+	color_rect.visible = false
+	
+	
+func _on_animation_player_animation_finished(anim_name):
+	print("oh yeah")
+	if (anim_name == "fade_in"):
+		on_transition_finished.emit()
+		animation_player.play("fade_out")
+	elif (anim_name == "fade_out"):
+		color_rect.visible = false
+		
+
+func fade_out():
+	'''Fades from black to transparent'''
+	color_rect.visible = true
+	animation_player.play("fade_out")
+	print(animation_player.current_animation)
+	
+func fade_in():
+	'''Fades from transparent to black'''
+	color_rect.visible = true
+	animation_player.play("fade_in")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+
+func _on_animation_player_animation_started(anim_name):
+	print(anim_name)
