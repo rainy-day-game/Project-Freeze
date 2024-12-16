1f45a903ac523f8b10e470d2c6bc3fbb3f494642
Raf <rafay.b075@gmail.com>
2024-12-16 16:43:20 -0600

    added squat animation to player

--- Player.gd
+++ Player.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var squat_timer = $"Squat Timer"
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 var last_direction = 1
-
+var started_jumping = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -23,6 +24,7 @@ func _physics_process(delta):
 	
 	var direction = Input.get_axis("ui_left", "ui_right")
 	apply_movement_anim(direction)
+	apply_jump_anim(velocity)
 	move_and_slide()
 
 func apply_movement_anim(direction):
@@ -42,3 +44,26 @@ func apply_movement_anim(direction):
 			animated_sprite.play("idle right")
 		else:
 			animated_sprite.play("idle left")
+			
+func apply_jump_anim(velocity):
+	if (velocity.y < 0): # Moving up
+		if (!started_jumping):
+			squat_timer.start(0.1)
+			started_jumping = true
+		if (squat_timer.is_stopped()):
+			if (last_direction == 1):
+				animated_sprite.play("jump right up")
+			else:
+				animated_sprite.play("jump left up")
+		else:
+			if (last_direction == 1):
+				animated_sprite.play("startup right")
+			else:
+				animated_sprite.play("startup left")
+	elif (velocity.y > 0):
+		if (last_direction == 1):
+			animated_sprite.play("fall right down")
+		else:
+			animated_sprite.play("fall left down")
+	else:
+		started_jumping = false
